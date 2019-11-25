8a864902c19a4c3d6365e505ccb4f7d2cde67335
Leonardo Silva <leonardosilvaif@hotmail.com>
2019-11-24 21:40:37 -0300

    adicionando fontes

---
+++ Enfermaria/src/main/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Classe Main.
+ */
+package main;
+
+import login.TelaLogin;
+import principal.TelaPrincipal;
+
+/**
+ *
+ * @author Franciele Alves Barbosa e Rogério Costa Negro Rocha
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TelaLogin meuLogin = new TelaLogin();
+        meuLogin.setVisible(true);
+
+        //TelaPrincipal telaPrincipal= new TelaPrincipal();
+        //telaPrincipal.setVisible(true);
+    }
+}
