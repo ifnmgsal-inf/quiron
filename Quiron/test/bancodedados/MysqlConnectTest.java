8ca3710a004062b31e0a9d078a882534134b2e9a
Rogério Costa <57320699+rogerio-costa@users.noreply.github.com>
2021-05-21 11:52:07 -0300

    Testes

---
+++ Quiron/test/bancodedados/MysqlConnectTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bancodedados;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author roger
+ */
+public class MysqlConnectTest {
+    
+    private MysqlConnect conexao;
+    
+    public MysqlConnectTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of connectDB method, of class MysqlConnect.
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testConnectDB() throws Exception {
+        assertEquals("com.mysql.jdbc.JDBC4Connection@6f79caec", conexao.connectDB());
+    }
+    
+}
